b3c9504e16494c33da029557fe5073680d93a85b
Aidan Matchette <aidan.j.matchette@gmail.com>
2023-01-29 20:16:04 -0800

    Implement direct messaging
    When a user uses the cmd `msg` followed by a user name, a message will
    be sent to that user.

--- ChatServer/src/main/java/com/aidan/ServerWorker.java
+++ ChatServer/src/main/java/com/aidan/ServerWorker.java
@@ -43,6 +43,11 @@ public class ServerWorker extends Thread {
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
+                } else if ("msg".equalsIgnoreCase(cmd)) {
+                    String[] tokensMsg = StringUtils.split(line, null, 3);
+                    handleMessage(tokensMsg);
+                } else if ("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
                 } else {
                     String msg = "unknown " + cmd + "\n";
                     outputStream.write(msg.getBytes());
@@ -51,6 +56,26 @@ public class ServerWorker extends Thread {
 
         }
         clientSocket.close();
+    }
+
+    private void handleJoin(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+        }
+    }
+
+    //format: "msg" "login" content...
+    private void handleMessage(String[] tokens) throws IOException {
+        String sendTo = tokens[1];
+        String msgContent = tokens[2];
+
+        List<ServerWorker> workerList = server.getWorkerList();
+        for (ServerWorker worker: workerList) {
+            if (sendTo.equalsIgnoreCase(worker.getLogin())) {
+                String outMsg = "msg " + login +" " + msgContent + "\n";
+                worker.send(outMsg);
+            }
+        }
     }
 
     private void handleLogoff() throws IOException{
